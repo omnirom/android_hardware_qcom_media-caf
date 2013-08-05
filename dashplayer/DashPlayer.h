40b342ac1a175bcc7f6322d6117d8303069da4f7
Jake Whatley <jacob.whatley91@gmail.com>
2013-08-05 15:43:22 +0530

    Add CAF to omnirom [1/5]
    for obvious reasons.
    
    dashplayer: Remove cached formats from the proper location
    
    1. Removed cached format/crop from the 0th position.
    2. Clear cached formats in destructor.
    
    Change-Id: Idd9c767bf9f660fe8ceae7f99b3ac51861b2a932
    
    mm-video: vdec: ref count the notifications to secure-display
    
    Notifications to display HAL to secure/unsecure display is sent
    when component is created/destroyed. When multiple instances
    are created/destroyed, the shared flag which indicates status
    of such notification can go out-of-sync.
    This change ref-counts the secure status flag and protects it
    with a lock.
    
    Change-Id: I126c92d16b03819afcbaf2f056928ba792556ed8
    
    mm-video: vidc: Fix an issue in flush for Surfacemediasource_test
    
    When i/p flush is in progress, need to give back all the ETB's
    to client as EBD's directly instead of post_event. so do not call
    push_input_buffer function.
    
    Change-Id: Id17a84e9a009bbe80ec034c78a78a08b889c394a
    CRs-Fixed: 517599
    
    camera bringup changes hardware-qcom-media
    
    Change-Id: I4b81d2150e5bbd97d09c91f3cf89a8c6169b6058
    
    Dashplayer: enable adaptive playback based on codec format key
    
    Added support for prepareForAdaptivePlayback() call in
    configureCodec, if max-width and max-height keys are specified
    in the format.  It is OK for this call to fail, if component
    does not implement adaptive playback.
    
    Change-Id: I403e70559cd54489d35a94b467192701f163d568
    
    mm-video: vdec: Support adaptive playback mode
    
    In this mode client checks and sets an extension to indicate
    that the content can change resolution, within the pre-
    announced maximum height/width. Component over-allocates
    buffers for the max resolution and dbp, and notifies client
    of any crop change when resolution changes within bounds.
    
    Resolution changes beyond set max width/height will result in
    port-reconfiguration notification, while adapting to the new
    resolution.
    
    Change-Id: I744ac475f3bbedaaca2b057e9a327243674f0d9e
    
    mm-video: vidc: set stride in gralloc-metadata for adaptive-playback
    
    In Adaptive-playback mode, following resolution change, decoder
    continues to decode on larger buffer. But the stride is changed
    implicitly. Set the updated stride in gralloc-metadata in each
    buffer, so HWC/GPU can read the stride to interpret the buffer's
    geometry correctly.
    
    Change-Id: If223e4bf230f8032b03b9d51d41b39576d0cf961
    
    dashplayer: Add support for invoke
    
    Add support for  invoke() in dashplayer and QCMediaPlayer
    
    Change-Id: I58ac0da2a9cd067d2ef92084b9c4c503786edb2c
    
    Fix compilation issues.
    
     * Only build when TARGET_QCOM_MEDIA_VARIANT=caf
     * Update makefiles for media-caf path
     * Add ifdefs for QCOM_BSP features
    
    Change-Id: I5a7c3ce1fd7334cb9af04ee27c6efe0620ebae09
    
    dashplayer: Handle error for text track
    
    Handle UNKNOWN_ERROR for text track
    
    Change-Id: Ifeb827b4b7f0fb796593789b0b51be333292c66f
    
    Remove AOSP reference to utils.mk
    
    Remove function calls from AOSP code to utils.mk
    & replace them with actual definition.
    
    Change-Id: I5f9bee27521c39caab17b473059729800e55314e
    CRs-fixed: 566287
    
    Conditionally revert Adaptive playback commits
    
    Dashplayer: enable adaptive playback based on codec format key
    mm-video: vdec: Support adaptive playback mode
    mm-video: vidc: set stride in gralloc-metadata for adaptive-playback
    
    This fixes netflix playback on several devices.
    Set TARGET_NO_ADAPTIVE_PLAYBACK := true to use
    
    Change-Id: Ie68fb9f8ec2122f2fc6e6654d9fdbc773dff509a
    
    Fix path for including non caf display variant files
    
    Change-Id: Iba960c34c185593bd0afb952a05f603aa5bf0db6
    
    dashplayer: Reject https dash urls
    
    Reject https dash urls
    
    Change-Id: Ib26edfdcd693fe7567b89adf488a48f349ca7bfe
    
    100% Sync with CM
    
    Ifdef items that depend on QCOM_BSP
    
    Change-Id: Ia4c1ea61f07e3d5c27d73b4c3f7851eaeb0fdef6
    
    Change-Id: I3fc567de3562f451c6303aea6bd6383fb1560687

--- dashplayer/DashPlayer.h
+++ dashplayer/DashPlayer.h
@@ -23,8 +23,9 @@
 #include <media/stagefright/NativeWindowWrapper.h>
 #include "DashPlayerStats.h"
 #include <media/stagefright/foundation/ABuffer.h>
-#define KEY_DASH_ADAPTION_PROPERTIES 8002
+#define KEY_DASH_ADAPTION_PROPERTIES 8002 // used for Get Adaotionset property
 #define KEY_DASH_MPD_QUERY           8003
+#define KEY_DASH_SET_ADAPTION_PROPERTIES 8004 // used for Set Adaotionset property
 
 namespace android {
 
@@ -41,7 +42,7 @@ struct DashPlayer : public AHandler {
 
     void setDataSource(const sp<IStreamSource> &source);
 
-    void setDataSource(
+    status_t  setDataSource(
             const char *url, const KeyedVector<String8, String8> *headers);
 
     void setDataSource(int fd, int64_t offset, int64_t length);
