6fd23369f7faf4cbfe2c181319628ab4a13b9ba2
Jon Haus <haus.xda@gmail.com>
2013-11-11 23:04:53 -0500

    media_caf: don't build qcmediaplayer if not used
    Building this will break some builds and is a waste of time if it's not going to be used.
    
    Change-Id: Ie8d7cbecf0b463a5692d43d5212b43b3c7e86303

--- QCMediaPlayer/Android.mk
+++ QCMediaPlayer/Android.mk
@@ -3,6 +3,8 @@
 #*brief Rules for copiling the source files
 #*  ******************************************************************************/
 
+ifneq ($(filter qcmediaplayer,$(PRODUCT_PACKAGES)),)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -13,3 +15,5 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_MODULE := qcmediaplayer
 LOCAL_MODULE_PATH := $(TARGET_OUT_JAVA_LIBRARIES)
 include $(BUILD_JAVA_LIBRARY)
+
+endif
